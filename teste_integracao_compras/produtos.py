a7bc8c83de7774efb2dd3afe3880c14d89e28b39
Aluno <aluno@debian.ifrn.local>
2025-07-03 16:25:33 -0300

    atividade terminada

--- teste_integracao_compras/produtos.py
+++ teste_integracao_compras/produtos.py
@@ -5,6 +5,7 @@ def cadastrar_produto(codigo, nome, preco):
     if codigo in produtos_db:
         raise ValueError("Produto já existe.")
     produtos_db[codigo] = {"nome": nome, "preco": preco}
+    return True
 
 def consultar_produto(codigo):
     return produtos_db.get(codigo)
--- teste_integracao_compras/teste_integracao.py
+++ teste_integracao_compras/teste_integracao.py
@@ -1,7 +1,13 @@
 
 from usuarios import cadastrar_usuario,consultar_usuario, usuarios_db
-from produtos import cadastrar_produto, produtos_db
+from produtos import cadastrar_produto,consultar_produto, produtos_db
 from compras import realizar_compra, listar_compras, compras_db
+
+def validacao(user):
+    if user == []:
+        print(f"a pessoa não possui compras registradas")
+    else:
+        print(f"retorno da consulta = {user}")
 
 def test_fluxo_integra_compras():
     print("\nIniciando teste de integração do sistema de compras...\n")
@@ -9,6 +15,8 @@ def test_fluxo_integra_compras():
      # Limpa os "bancos" em memória
 
     usuarios_db.clear()
+    produtos_db.clear()
+    compras_db.clear()
 
 
     # 1. Cadastrar usuários e testar
@@ -16,26 +24,60 @@ def test_fluxo_integra_compras():
     assert cadastrar_usuario(1, "vivian")
     assert cadastrar_usuario(2, "eduarda")
     assert cadastrar_usuario(3, "pricila")
+    assert cadastrar_usuario(4, "luana")
+    assert cadastrar_usuario(5, "marcia")
 
     user = consultar_usuario(3)
     assert user == "pricila", f"Esperado 'pricila', obtido {user}"
-    print(f"retorno da consulta = {user}")
+    print(f"retorno da consulta Usuario = {user}")
 
 
     # 2. Cadastrar produtos e testar
 
+    assert cadastrar_produto(1, "macarrão", 2.5)
+    assert cadastrar_produto(2, "cuscuz", 1.99)
+    assert cadastrar_produto(3, "pippos", 3.90)
+
+    user = consultar_produto(2)
+    validacao(user)
+    assert user == {'nome': 'cuscuz', 'preco': 1.99}, f"Esperado 'cuscuz', obtido {user}"
+    
 
     # 3. Realizar compra válida
+
+    assert realizar_compra(1, {1,2})
+    assert realizar_compra(2, {3,2})
+
+    user = listar_compras(2)
+    validacao(user)
+
+    user = listar_compras(1)
+    validacao(user)
+    assert user == [{'produtos': {1, 2}, 'total': 4.49}], f"Esperado '[{'produtos': {1, 2}, 'total': 4.49}]', obtido {user}"
 
 
     # 4. Tentar compra com produto inválido
 
+    assert realizar_compra(3, {2,6})
+
+    user = listar_compras(3)
+    print(f"retorno da consulta compra = {user}")
+
+    assert user == [{'produtos': {1, 2}, 'total': 4.49}], f"Esperado '[{'produtos': {1, 2}, 'total': 4.49}]', obtido {user}"
+
+
 
     # 5. Verificar compras do usuário 1
 
+    user = listar_compras(1)
+    validacao(user) 
+    assert user == [{'produtos': {1, 2}, 'total': 4.49}], f"Esperado '[{'produtos': {1, 2}, 'total': 4.49}]', obtido {user}"
 
     # 6. Verificar um usuário que não tem compras
 
+    validacao(listar_compras(3)) 
+
+    
 
 # Executa o teste
 if __name__ == "__main__":
